f00cfa901974bf63bb931005f80ff5121d228243
sharnack1 <32234951+sharnack1@users.noreply.github.com>
2017-09-25 00:13:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,8 @@ if (!is_null($events['events'])) {
 			$command = substr($command,0,2);
 			$id = $text;
 			$id = substr($text,2);
-			include("http://www.smart-mom.org/botevent?id='.$id.'");
+			$url = "http://www.smart-mom.org/botevent.php?id=".$id;
+			include($url);
 
 			
 			if($command =="->")
